433ce0c8b025f364f8a75b6175668ad1cb921d41
sevenearths <from_a_far@hushmail.com>
2021-03-31 16:38:14 +0100

    testing populate_cms in the testing class constructor

--- courses/test_files/test_courses.py
+++ courses/test_files/test_courses.py
@@ -11,8 +11,9 @@ from django.conf import settings
 @tag('github')
 class CoursesTestCase(TestCase):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self):
+        super().__init__()
+        management.call_command('populate_cms')
 
     def test_front_page(self):
 
@@ -20,18 +21,6 @@ class CoursesTestCase(TestCase):
         self.assertEqual(200, response.status_code)
 
     def test_LLB_LAW(self):
-
-        management.call_command('populate_cms')
-
-        print('---')
-        print(settings.DATABASES['default'])
-        print('---')
-
-        page_count = Page.objects.count()
-
-        print('+++')
-        print(page_count)
-        print('+++')
 
         response = self.client.get('http://localhost/course-details/10007804/U18-LAWLLB/Full-time/')
         self.assertEqual(200, response.status_code)
