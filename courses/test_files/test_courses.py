6bf0f545c6d3b4918fd4d61ddd4be247ea61c38f
sevenearths <from_a_far@hushmail.com>
2021-03-31 09:56:42 +0100

    added testing a single course

--- courses/test_files/test_courses.py
+++ courses/test_files/test_courses.py
@@ -11,3 +11,8 @@ class CoursesTestCase(BaseTestCase):
 
         response = self.client.get('/')
         self.assertEqual(200, response.status_code)
+
+    def testrs_LLB_LAW(self):
+
+        response = requests.get('http://localhost:8000/course-details/10007804/U18-LAWLLB/Full-time/')
+        self.assertEqual(200, response.status_code)
