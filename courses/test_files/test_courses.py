67d25441fe510c93ed659c093bb3ad0c7efa2574
sevenearths <from_a_far@hushmail.com>
2021-03-31 14:47:01 +0100

    (printing out values)

--- courses/test_files/test_courses.py
+++ courses/test_files/test_courses.py
@@ -2,6 +2,8 @@ import sys
 
 from django.test import tag
 
+from wagtail.core.models import Page
+
 from core.tests import BaseTestCase
 from courses.models import CourseManagePage
 
@@ -23,6 +25,12 @@ class CoursesTestCase(BaseTestCase):
         print('---')
         print(settings.DATABASES['default'])
         print('---')
+
+        page_count = Page.objects.count()
+
+        print('+++')
+        print(page_count)
+        print('+++')
 
         response = self.client.get('http://localhost/course-details/10007804/U18-LAWLLB/Full-time/')
         self.assertEqual(200, response.status_code)
