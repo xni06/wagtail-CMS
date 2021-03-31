ba67e51e3edda563386a253f521bd836c6c697d7
sevenearths <from_a_far@hushmail.com>
2021-03-31 14:28:53 +0100

    (printing out values)

--- courses/test_files/test_courses.py
+++ courses/test_files/test_courses.py
@@ -1,7 +1,11 @@
+import sys
+
 from django.test import tag
 
 from core.tests import BaseTestCase
 from courses.models import CourseManagePage
+
+from django.conf import settings
 
 
 @tag('github')
@@ -13,6 +17,14 @@ class CoursesTestCase(BaseTestCase):
         self.assertEqual(200, response.status_code)
 
     def testrs_LLB_LAW(self):
+
+        print('---')
+        print(sys.argv)
+        print('---')
+        print(settings.DBHOST)
+        print('---')
+        print(DATABASES['default']['ENGINE'])
+        print('---')
 
         response = self.client.get('http://localhost/course-details/10007804/U18-LAWLLB/Full-time/')
         self.assertEqual(200, response.status_code)
